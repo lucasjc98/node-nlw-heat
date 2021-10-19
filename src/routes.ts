c17be25be1fd7836408f86334b2b4f7343b746cc
Lucas Costa <lucasjcosta.98@gmail.com>
2021-10-19 14:28:39 -0300

    Add JWT, cors and socket.io

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/Get3LastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -12,5 +14,9 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
